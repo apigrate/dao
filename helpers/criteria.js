db0693b99f9380b3a6da7aee9b7de55e7c1d917a
gaumeister <derek@apigrate.com>
2019-07-26 07:48:15 -0500

    v2.3.0 criteria refactoring

--- helpers/criteria.js
+++ helpers/criteria.js
@@ -16,140 +16,140 @@ const _ = require('lodash');
   }
 
 */
-function CriteriaHelper(opts){
-
-  this.whereClause='';
-  this.parms=[];
-  this.queryOptions={};
-  this.omitNull=opts && opts.omitIfNull || true;
-  this.omitEmpty=opts && opts.omitIfEmpty || true;
-
-};
-
-/**
-  Chainable function to inject AND criteria into a where clause. Typically used by controllers
-  accepting parameters from the end user.
-  @param {string} name column name
-  @param {string} oper valid sql operator (=,<,>,<=,>=,LIKE,IS NOT NULL, IS NULL)
-  @param {object} the value
-*/
-CriteriaHelper.prototype.and = function(name, oper, value){
-  var self = this;
-  return self._where('AND', name, oper, value)
-};
-
-/**
-  Chainable function to inject OR criteria into a where clause. Typically used by controllers
-  accepting parameters from the end user.
-  @param {string} name column name
-  @param {string} oper valid sql operator (=,<,>,<=,>=,LIKE,IS NOT NULL, IS NULL)
-  @param {object} the value
-*/
-CriteriaHelper.prototype.or = function(name, oper, value){
-  var self = this;
-  return self._where('OR', name, oper, value)
-};
-
-/**
-  Chainable function to inject a criteria into where clause. Typically used by controllers
-  accepting parameters from the end user.
-  @param {string} bool boolean operator AND, OR
-  @param {string} name column name
-  @param {string} oper valid sql operator (=,<,>,<=,>=,LIKE,IS NOT NULL, IS NULL)
-  @param {object} the value
-*/
-CriteriaHelper.prototype.where = function(bool, name, oper, value){
-  var self = this;
-  return self._where(bool, name, oper, value)
-};
-
-/**
-  Chainable function to add an order by criteria. Typically used by controllers
-  accepting parameters from the end user.
-  @param {array} columns with +- prepended indicating ascending/desceding order
-  (e.g. +name equivalent to name ASC, -created equivalent to created DESC)
-*/
-CriteriaHelper.prototype.orderBy = function(columns){
-  var self=this;
-  if(_.isEmpty(columns)){
-    return self;
+class CriteriaHelper{
+  constructor(opts){
+    this.whereClause='';
+    this.parms=[];
+    this.queryOptions={};
+    this.omitNull=opts && opts.omitIfNull || true;
+    this.omitEmpty=opts && opts.omitIfEmpty || true;
   }
-  self.queryOptions.orderBy=columns;
-  return self;
-};
 
-/**
-  Chainable function to add a row limit criteria. Typically used by controllers
-  accepting parameters from the end user.
-  @param {integer} number
-*/
-CriteriaHelper.prototype.limit = function(number){
-  var self=this;
-  if(_.isNil(number) || number < 0){
-    self.queryOptions.limit=1;
-    return self;
+
+  /**
+    Chainable function to inject AND criteria into a where clause. Typically used by controllers
+    accepting parameters from the end user.
+    @param {string} name column name
+    @param {string} oper valid sql operator (=,<,>,<=,>=,LIKE,IS NOT NULL, IS NULL)
+    @param {object} the value
+  */
+  and(name, oper, value){
+
+    return this._where('AND', name, oper, value)
   }
-  self.queryOptions.limit=Math.floor(number);
-  return self;
-};
 
-/**
-  Chainable function to add a row offset criteria. Typically used by controllers
-  accepting parameters from the end user.
-  @param {integer} number
-*/
-CriteriaHelper.prototype.offset = function(number){
-  var self=this;
-  if(_.isNil(number) || number < 0){
-    self.queryOptions.offset=0;
-    return self;
+  /**
+    Chainable function to inject OR criteria into a where clause. Typically used by controllers
+    accepting parameters from the end user.
+    @param {string} name column name
+    @param {string} oper valid sql operator (=,<,>,<=,>=,LIKE,IS NOT NULL, IS NULL)
+    @param {object} the value
+  */
+  or(name, oper, value){
+
+    return this._where('OR', name, oper, value)
   }
-  self.queryOptions.offset=Math.floor(number);
-  return self;
-};
 
-//Internal use
-CriteriaHelper.prototype._where = function(bool, name, oper, value){
-  var self = this;
+  /**
+    Chainable function to inject a criteria into where clause. Typically used by controllers
+    accepting parameters from the end user.
+    @param {string} bool boolean operator AND, OR
+    @param {string} name column name
+    @param {string} oper valid sql operator (=,<,>,<=,>=,LIKE,IS NOT NULL, IS NULL)
+    @param {object} the value
+  */
+  where(bool, name, oper, value){
 
-  oper = oper.toUpperCase();
+    return this._where(bool, name, oper, value)
+  }
 
-  //Some operators are unary
-  if(oper!=='IS NOT NULL' && oper!=='IS NULL'){
+  /**
+    Chainable function to add an order by criteria. Typically used by controllers
+    accepting parameters from the end user.
+    @param {array} columns with +- prepended indicating ascending/desceding order
+    (e.g. +name equivalent to name ASC, -created equivalent to created DESC)
+  */
+  orderBy(columns){
 
-    if(_.isNil(value) && self.omitNull){
-      return self;
+    if(this.isEmpty(columns)){
+      return this;
     }
-    if(isEmpty(value) && self.omitEmpty){
-      return self;
+    this.queryOptions.orderBy=columns;
+    return this;
+  }
+
+  /**
+    Chainable function to add a row limit criteria. Typically used by controllers
+    accepting parameters from the end user.
+    @param {integer} number
+  */
+  limit(number){
+
+    if(_.isNil(number) || number < 0){
+      this.queryOptions.limit=1;
+      return this;
     }
+    this.queryOptions.limit=Math.floor(number);
+    return this;
   }
 
-  if(self.whereClause===''){
-    //Initial WHERE excluded by convention
-    self.whereClause += name + ' ' + oper;
-  } else {
-    self.whereClause += ' ' + bool + ' ' + name  + ' ' + oper;
+  /**
+    Chainable function to add a row offset criteria. Typically used by controllers
+    accepting parameters from the end user.
+    @param {integer} number
+  */
+  offset(number){
+
+    if(_.isNil(number) || number < 0){
+      this.queryOptions.offset=0;
+      return this;
+    }
+    this.queryOptions.offset=Math.floor(number);
+    return this;
   }
 
-  if(oper==='IS NOT NULL' || oper==='IS NULL'){
-    return self;
+  //Internal use
+  _where(bool, name, oper, value){
+
+    oper = oper.toUpperCase();
+
+    //Some operators are unary
+    if(oper!=='IS NOT NULL' && oper!=='IS NULL'){
+
+      if(_.isNil(value) && this.omitNull){
+        return this;
+      }
+      if(this.isEmpty(value) && this.omitEmpty){
+        return this;
+      }
+    }
+
+    if(this.whereClause===''){
+      //Initial WHERE excluded by convention
+      this.whereClause += name + ' ' + oper;
+    } else {
+      this.whereClause += ' ' + bool + ' ' + name  + ' ' + oper;
+    }
+
+    if(oper==='IS NOT NULL' || oper==='IS NULL'){
+      return this;
+    }
+
+    if(oper==='LIKE'){
+      this.whereClause += ' ?';//extra space is significant.
+      this.parms.push( value );
+    } else {
+      this.whereClause += '?';
+      this.parms.push( value );
+    }
+
+    return this;
   }
 
-  if(oper==='LIKE'){
-    self.whereClause += ' ?';//extra space is significant.
-    self.parms.push( value );
-  } else {
-    self.whereClause += '?';
-    self.parms.push( value );
+  isEmpty(x){
+    return _.isNil(x) || (!_.isArray(x) && x.length===0);
+    x = x+'';
+    return x.trim() === '';
   }
-
-  return self;
-};
-
-function isEmpty(x){
-  return _.isNil(x) || (!_.isArray(x) && x.length===0);
-  x = x+'';
-  return x.trim() === '';
 }
 module.exports=CriteriaHelper;
